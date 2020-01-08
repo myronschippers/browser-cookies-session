4a31be55f2b0204904be144ad52845072b0b2b6f
Myron <dubtwee@gmail.com>
2020-01-08 15:53:13 -0600

    removing dependency that is no longer being used

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import { setCookie, getCookie } from '../../services/cookies';
 import axios from 'axios';
 
 class App extends Component {
